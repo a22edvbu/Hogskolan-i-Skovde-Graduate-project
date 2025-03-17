f7a069599e6f6ab24b232ca10b102042376f13ca
Edvin Bülow <a22edvbu@student.his.se>
2025-03-17 12:31:32 +0100

    Initialized created the different pages for each database

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GaZoline Database Services</title>
+</head>
+<body>
+    <h1> GaZoline Database Services </h1>
+    <p> De coolaste databaserna known to man!</p>
+    <div class="mongoDB"><a href="mongodb.php">MongoDB</a></div>
+    <div class="MySQL"><a href="mysql.php">MySQL</a></div>
+</body>
+</html>
