959444f92c98e3da359ff3f62c9b665cdc895726
Edvin Bülow <a22edvbu@student.his.se>
2025-03-21 13:43:32 +0100

    Added insert form to mongodb.php

--- mongodb.php
+++ mongodb.php
@@ -7,9 +7,31 @@
     <title>MongoDB Database</title>
 </head>
 <body>
+    <?php
+
+    ?>
     <h1>MongoDB</h1>
     <p>
         <div class="homeBtn"><a href="index.php">Home</a></div>
     </p>
+    <h2>
+        MongoDB Database:
+    </h2>
+    <h3>Insert:</h3>
+    <form action='mongodb.php' method='POST' id="mongoPostForm">
+        <label for="mongoName">Name: </label>
+        <input type="text" name="mongoName" id="mongoName">
+
+        <label for="mongoID">ID: </label>
+        <input type="text" name="mongoID" id="mongoID">
+        
+        <label for="mongoBday">Birthday: </label>
+        <input type="text" name="mongoBday" id="mongoBday">
+        
+        <label for="mongoNotes">Notes: </label>
+        <input type="text" name="mongoNotes" id="mongoNotes">
+
+        <input type="submit" value="Insert">
+    </form>
 </body>
 </html>
